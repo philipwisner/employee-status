d769a0933760c7ade4d886ff1fc631e3135876ff
Philip Wisner <philipwisner@gmail.com>
2019-08-27 09:40:37 -0400

    added filter bar

---
+++ src/components/Filterbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="filter-bar">
+    <div class="channel">
+      <select name="channel" id="channel">
+        <option value="sn_devs">Select a channel</option>
+      </select>
+    </div>
+    <div class="color-mode">
+      <button v-if="!darkMode" @click="setDarkMode(true)"> <img class="moon" src="../assets/moon.svg" alt="moon"></button>
+      <button v-if="darkMode" @click="setDarkMode(false)"><img src="../assets/sun.svg" alt="sun"></button>
+    </div>
+    <div class="filter-menu">
+      <select name="filter" id="filter">
+        <option default>Filter Status</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Filterbar',
+  props: {
+    darkMode: Boolean
+  },
+  methods: {
+    setDarkMode(mode) {
+      this.$emit('update-dark-mode', mode);
+      console.log('mode', mode);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.filter-bar {
+  margin-top: 20px;
+  margin-bottom: 5px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.channel {
+  flex: 1 1 auto;
+  text-align: left;
+}
+#channel {
+  min-width: 160px;
+}
+select {
+  background: none;
+  border: none;
+  font-size: 16px;
+}
+.color-mode {
+  flex: 0 1 auto;
+}
+.filter-menu {
+  flex: 0 1 auto;
+  min-width: 150px;
+  text-align: right;
+}
+button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  top: 2px;
+}
+.moon {
+  width: 15px;
+}
+</style>
