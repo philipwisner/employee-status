166a55c5f3417cf5b819d2ec2e41b8f2d43e2a27
Philip Wisner <philipwisner@gmail.com>
2019-12-12 00:10:59 -0500

    update

--- src/router.js
+++ src/router.js
@@ -1,23 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Library from './views/Library.vue'
+import Categories from "./views/Categories.vue";
+import Artists from "./views/Artists.vue";
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "library",
+      component: Library
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: "/categories",
+      name: "categories",
+      component: Categories
+    },
+    {
+      path: "/artists",
+      name: "artists",
+      component: Artists
+    },
+    {
+      path: "/library",
+      name: "library",
+      component: Library
+    },
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // }
   ]
-})
+});
